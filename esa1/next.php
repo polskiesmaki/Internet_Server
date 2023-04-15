95541b09bed5a707eaefa333813b6056004d1c9b
Denio <de.glowacki@ostfalia.de>
2023-04-15 17:02:38 +0200

    new

--- esa1/next.php
+++ esa1/next.php
@@ -9,20 +9,39 @@
 		 <ul>
 			<li><?php echo"<a href='index.php'>Main</a>"; ?></li>
 			<li><?php echo"<a href='next.php'>Projektkontakt</a>"; ?></li>
+			<li><?php echo"<a href='next2.php'>Fill Data</a>"; ?></li>
+			
 		 </ul>
 		</nav>
 		
 		<section>
 		<form action = ''> 
 			 
-			<p><label>Vorname <input id='vname' type='text'  placeholder='Texteingabe' required /></label></p>
-			<p><label>Nachname <input id='nname' type='text'required placeholder='Texteingabe' /></label></p>
-			
-			<p><label>Telefonnummer <input id='tnr' type='tel' placeholder="Eingabe v. Ortsvorwahl und Anschluss"/></label></p>
-			<p><label>E-Mail <input id='email' type='email' placeholder="Eingabe einer E-Mailadresse" required /></label></p>
-			
-			<p><label>Firma:  <input id='firma' value='<?php echo"Ostfalia"; ?>' type='text'/></label></p>
-			<button type='button' onclick='alert("Hallo Welt")'>Klick mich</button>
+		<?php
+		// Get the selected index from the URL
+		$selected_index = $_GET['personen'];
+
+		// Load the contents of data.json into a string variable
+		$json_string = file_get_contents('data.json');
+
+		// Parse the JSON string into a PHP object
+		$json_object = json_decode($json_string);
+
+		// Get the value of "vname" from the parsed JSON object for the selected person
+		$vname = $json_object->personen[$selected_index]->vorname;
+		$nname = $json_object->personen[$selected_index]->nachname;
+		$email = $json_object->personen[$selected_index]->email;
+		$tnr = $json_object->personen[$selected_index]->telefonnummer;
+
+		// Output the HTML code with the input field pre-filled with the value of "vname"
+		//echo "<p><label>Vorname <input id='vname' type='text' placeholder='Texteingabe' required value='$vname' /></label></p>";
+
+		echo "<p><label>Vorname <input id='vname' type='text' placeholder='Texteingabe' required value='$vname' /></label></p>";
+		echo "<p><label>Nachname <input id='nname' type='text' placeholder='Texteingabe' required value='$nname' /></label></p>";
+		echo "<p><label>E-Mail <input id='email' type='email' placeholder='Eingabe einer E-Mailadresse' required value='$email' /></label></p>";
+		echo "<p><label>Telefonnummer <input id='tnr' type='tel' placeholder='Eingabe v. Ortsvorwahl und Anschluss' required value='$tnr' /></label></p>";
+		
+?>			
 			<input type='submit' value='Submit'/>
 		</form>
 		</section>
---
+++ esa1/next2.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Projektkontakt</title>
+	</head>
+	<body>
+		<header> <h1>Projektkontakt</h1> </header>
+		<nav>
+		 <ul>
+			<li><?php echo"<a href='index.php'>Main</a>"; ?></li>
+			<li><?php echo"<a href='next.php'>Projektkontakt</a>"; ?></li>
+			<li><?php echo"<a href='next2.php'>Fill Data</a>"; ?></li>
+			
+		 </ul>
+		</nav>
+		
+		<section>
+		
+        <form action="next.php" method="GET">
+  <label for="personen">Choose a person:</label>
+  <select id="personen" name="personen">
+    <option value="0">1</option>
+    <option value="1">2</option>
+    <option value="2">3</option>
+    <option value="3">4</option>
+    <option value="4">5</option>
+  </select>
+  <button type="submit">Next</button>
+</form>
+		</section>
+	</body>
+</html>
+
+
