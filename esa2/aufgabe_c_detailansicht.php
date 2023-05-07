3b9a13552595a72ffed76db03ccc876c0b7fc488
Miakosa <130900759+Miakosa@users.noreply.github.com>
2023-05-07 18:52:01 +0200

    Update aufgabe_c_detailansicht.php

--- esa2/aufgabe_c_detailansicht.php
+++ esa2/aufgabe_c_detailansicht.php
@@ -1,101 +1,55 @@
 <?php
-// Laden der Daten aus der JSON-Datei
-$data = json_decode(file_get_contents('data.json'), true);
+$json_data = file_get_contents('data.json');
+$data = json_decode($json_data, true);
 
-// Verarbeiten des Formulars für das Erstellen eines neuen Kontakts
-if (isset($_POST['create'])) {
-    $person = array(
-        'vorname' => $_POST['vorname'],
-        'nachname' => $_POST['nachname'],
-        'telefon' => $_POST['telefon'],
-        'email' => $_POST['email']
-    );
-    array_push($data['personen'], $person);
-    file_put_contents('data.json', json_encode($data));
-    header('Location: aufgabe_c.php');
-    exit;
-}
+$id = $_GET['id'];
 
-// Verarbeiten des Formulars für das Bearbeiten eines Kontakts
-if (isset($_POST['update'])) {
-    $person = array(
-        'vorname' => $_POST['vorname'],
-        'nachname' => $_POST['nachname'],
-        'telefon' => $_POST['telefon'],
-        'email' => $_POST['email']
-    );
-    $data['personen'][$_POST['id']] = $person;
-    file_put_contents('data.json', json_encode($data));
-    header('Location: aufgabe_c.php');
-    exit;
-}
-
-// Verarbeiten des Löschens eines Kontakts
-if (isset($_GET['delete'])) {
-    $data = json_decode(file_get_contents('data.json'), true);
-    array_splice($data['personen'], $_GET['delete'], 1);
-    file_put_contents('data.json', json_encode($data));
-    header('Location: aufgabe_c.php');
-    exit;
-}
+$person = $data['personen'][$id];
 ?>
-
 
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Projektkontakt</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Projektkontakt</title>
 </head>
 
 <body>
-    <header>
-        <h1>Projektkontakte Liste</h1>
-    </header>
-	<nav>
-	<a href="index.html">Zurück</a><br>
-	</nav>
-    <table id="personen-tabelle">
-        <thead>
-            <tr>
-                <th>Name</th>
+	<header>
+		<h1>Projektkontakt</h1>
+	</header>
+	<div>
+		<h2>
+			<?php echo $person['vorname'] . ' ' . $person['nachname']; ?>
+		</h2>
+		<form action="" method="post">
+			<p>
+				<label for="vorname">Vorname:</label>
+				<input readonly type="text" id="vorname" name="vorname" value="<?php echo $person['vorname']; ?>"
+					required>
+			</p>
+			<p>
+				<label for="nachname">Nachname:</label>
+				<input readonly type="text" id="nachname" name="nachname" value="<?php echo $person['nachname']; ?>"
+					required>
+			</p>
+			<p>
+				<label for="email">E-Mail:</label>
+				<input readonly type="email" id="email" name="email" value="<?php echo $person['email']; ?>" required>
+			</p>
+			<p>
+				<label for="telefonnummer">Telefonnummer:</label>
+				<input readonly type="tel" id="telefonnummer" name="telefonnummer"
+					value="<?php echo $person['telefonnummer']; ?>" required>
+			</p>
+			<input type="hidden" name="id" value="<?php echo $id; ?>">
 
-            </tr>
-        </thead>
-        <tbody>
-            <?php $json_data = file_get_contents('data.json');
-            $data = json_decode($json_data, true);
-            ?>
-            <?php foreach ($data['personen'] as $key => $person) { ?>
-                <tr>
-                    <td><a href="aufgabe_c_detailansicht.php?id=<?php echo $key; ?>">
-                            <?php echo $person['nachname'] . ', ' . $person['vorname']; ?></a></td>
-                </tr>
-                <td>
-                    <form action="aufgabe_c_bearbeitenansicht.php?id=<?php echo $key; ?>" method="get"
-                        style="display: inline;">
-                        <input type="hidden" name="id" value="<?php echo $key; ?>">
-                        <button class="ausnahme" type="submit">
-                            <img src="img\bearbeiten.png" alt="Bearbeiten" title="Bearbeiten">
-                        </button>
-                    </form>
-                    <form action="aufgabe_c.php" method="get" style="display: inline;">
-                        <input type="hidden" name="delete" value="<?php echo $key; ?>">
-                        <button class="ausnahme" type="submit">
-                            <img src="img\loeschen.png" alt="Löschen" title="Löschen">
-                        </button>
-                    </form>
-                <?php } ?>
-        </tbody>
-    </table>
 
-    <form  action="aufgabe_c_erstellenansicht.php" method="get" style="display: inline;">
-        <input  type="hidden" name="create">
-        <button  class="ausnahme" type="submit">
-            <img src="img\plus.png" alt="Erstellen" title="Erstellen">
-        </button>
-    </form>
+			<button><a href="aufgabe_c.php">Zurück</a></button>
+
+		</form>
+
+	</div>
 </body>
-
 
 </html>
